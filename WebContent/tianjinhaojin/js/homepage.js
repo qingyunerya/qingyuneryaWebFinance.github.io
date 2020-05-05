85344262264e8271cdca487e833b65435163235f
qingyunerya <37932315+qingyunerya@users.noreply.github.com>
2020-05-05 18:22:24 +0800

    this is new

---
+++ WebContent/tianjinhaojin/js/homepage.js
@@ -0,0 +1,25 @@
+function check_kw() {if(Dd('kw').value == L['keyword_value'] || Dd('kw').value.length<2) {alert(L['keyword_message']); Dd('kw').focus(); return false;}}
+function Df(url, etc) {document.write('<iframe src="'+url+'" scrolling="no" frameborder="0" '+etc+'></iframe>');}
+function show_date() {
+	var dt_day = dt_month = dt_weekday = '';
+	var dt_week = [L['Sunday'], L['Monday'], L['Tuesday'], L['Wednesday'], L['Thursday'], L['Friday'], L['Saturday']];
+	dt_today = new Date();
+	dt_weekday = dt_today.getDay();
+	dt_month = dt_today.getMonth()+1;
+	dt_day = dt_today.getDate();
+	document.write(lang(L['show_date'], [dt_month, dt_day, dt_week[dt_weekday]]));
+}
+$(document).ready(function(){
+	$('.main_nav ul li').hover(function() {
+		//$(this).children().stop(false,true);
+		//$(this).addClass('hover');
+		
+		//$(this).children('.sub-ul').fadeIn();
+	},
+	function() {
+		//$(this).children().stop(false,true);
+		//$(this).removeClass('hover');
+		
+		//$(this).children('.sub-ul').hide();
+	});
+});
